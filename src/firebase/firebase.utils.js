103fdc142996b28d127c0ba57b2b7570ed3b08d5
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2021-08-26 22:38:05 +0300

    add sign-up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth'
+import { GoogleAuthProvider, signInWithPopup, getAuth } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const config = {
     apiKey: 'AIzaSyAk6UrOuTGFXd0l37X_ZC2FVUvu5gBHy88',
@@ -18,3 +19,30 @@ const provider = new GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
 
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
+
+const db = getFirestore();
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) {
+        return;
+    }
+
+    const docRef = doc(db, 'users', userAuth.uid);
+    const docSnap = await getDoc(docRef);
+    if (!docSnap.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(docRef, {
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return docRef;
+};
