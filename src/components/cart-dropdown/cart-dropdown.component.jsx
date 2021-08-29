c3bad9b397381fe01e01b8b40dd345c39d6d484f
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2021-08-29 22:27:02 +0300

    add selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import "./cart-dropdown.styles.scss";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
 import { connect } from "react-redux";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
 
 const CartDropdown = ({ items }) => (
   <div className="cart-dropdown">
@@ -15,6 +16,8 @@ const CartDropdown = ({ items }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { items } }) => ({ items });
+const mapStateToProps = (state) => ({
+  items: selectCartItems(state),
+});
 
 export default connect(mapStateToProps)(CartDropdown);
