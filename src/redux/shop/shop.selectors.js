15a860de4aff1983e2f178e883f97d62d34d9513
Dmitri Butorchin <Dmitri.Butorchin@gmail.com>
2021-09-07 18:42:45 +0300

    add collection page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,8 +1,21 @@
 import { createSelector } from "reselect";
+import memoize from "lodash.memoize";
 
 const selectShop = (state) => state.shop;
 
-export const selectShopCollections = createSelector(
+const selectShopCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
+);
+
+export const selectCollectionsForPreview = createSelector(
+  [selectShopCollections],
+  (collections) => Object.values(collections)
+);
+
+export const selectCollection = memoize((collectionId) =>
+  createSelector(
+    [selectShopCollections],
+    (collections) => collections[collectionId]
+  )
 );
